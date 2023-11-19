d078d9c5a55739481cfa4def96aaf7b77bf11425
heiler <threepebbles@naver.com>
2023-11-20 02:46:19 +0900

    Feat(Application): 메인 함수에서 게임 실행 하도록 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.MainController;
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        MainController mainController = new MainController(inputView, outputView);
+        mainController.run();
     }
 }

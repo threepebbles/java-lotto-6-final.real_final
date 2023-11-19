2a5b84c3a6c9207648721c337f8d1cfd753f52f5
heiler <threepebbles@naver.com>
2023-11-20 04:13:29 +0900

    Test: LottoMachine 테스트 코드 리팩토링

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,14 +2,13 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LottoMachineTest {
     @Test
     void 로또_개수_계산_테스트() {
         int money = 8000;
-        LottoMachine lottoMachine = new LottoMachine(new LottoNotRandomGenerator(List.of(1, 2, 3, 4, 5, 6)));
+        LottoMachine lottoMachine = new LottoMachine(new LottoRandomGenerator());
         int expectedCount = 8;
 
         int actualCount = lottoMachine.calculateQuantity(money);
@@ -20,11 +19,11 @@ public class LottoMachineTest {
     @Test
     void 금액에_맞게_로또_생성_하는지_테스트() {
         int money = 8000;
-        LottoMachine lottoMachine = new LottoMachine(new LottoNotRandomGenerator(List.of(1, 2, 3, 4, 5, 6)));
+        LottoMachine lottoMachine = new LottoMachine(new LottoRandomGenerator());
         int expectedCount = 8;
 
         Lottos lottos = lottoMachine.generateLottosByMoney(money);
-        
+
         assertThat(lottos.size()).isEqualTo(expectedCount);
     }
 }

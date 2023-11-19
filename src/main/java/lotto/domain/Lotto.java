6f091c56a34c4d1fa98a71a49ad43f4cbd19c774
heiler <threepebbles@naver.com>
2023-11-20 02:51:26 +0900

    Feat(Lotto): 로또 번호 유효성 검증 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,39 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.ErrorMessage;
 import lotto.view.output.dto.LottoDto;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplication(numbers);
+        numbers.forEach(this::validateInRange);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> numbers) {
+        final int LOTTO_SIZE = 6;
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("올바른 로또 번호 개수가 아닙니다."));
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if (numbers.stream()
+                .distinct()
+                .count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("중복된 번호가 있습니다."));
+        }
+    }
+
+    private void validateInRange(Integer number) {
+        final int LOTTO_NUMBER_MIN = 1;
+        final int LOTTO_NUMBER_MAX = 45;
+        if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("올바른 범위의 로또 번호가 아닙니다."));
         }
     }
 

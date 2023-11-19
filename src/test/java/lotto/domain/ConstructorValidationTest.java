8ecb2d04529b014567ae208256a7abeae1318546
heiler <threepebbles@naver.com>
2023-11-20 04:08:27 +0900

    Test: WinningNumbers 생성자 유효성 검증 테스트 코드 작성

--- src/test/java/lotto/domain/ConstructorValidationTest.java
+++ src/test/java/lotto/domain/ConstructorValidationTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ConstructorValidationTest {
     @Test
@@ -34,6 +36,33 @@ public class ConstructorValidationTest {
     void 로또_생성_예외_테스트_범위_밖_숫자() {
         List<Integer> numbers = List.of(0, 2, 3, 4, 5, 6);
         assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void 당첨_번호들_정상_생성_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        assertThatCode(() -> new WinningNumbers(winningLotto, bonusNumber))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 당첨_번호들_생성_예외_테스트_보너스번호_중복() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 6;
+        assertThatThrownBy(() -> new WinningNumbers(winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46, 50})
+    void 당첨_번호들_생성_예외_테스트_보너스번호가_범위_밖_숫자(int bonusNumber) {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> new WinningNumbers(winningLotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
